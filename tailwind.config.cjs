27b0640ca23a77ecb4973a430d1e6e792087d418
snell <sundell.benjamin@gmail.com>
2023-03-09 13:32:13 +0100

    feat: added prettier & changed tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ffffff',
+        secondary: 'rgb(241 245 249)',
+        tertiary: '#F7FBFD',
+        accent: '#000000ff',
+      },
+      textColor: {
+        primary: '#363848',
+        secondary: 'rgb(241, 245, 249)',
+        accent: '#00000000ff',
+      },
+      fontFamily: {
+        sourcesanspro: '"Source Sans Pro", sans-serif',
+      },
+      gridTemplateColumns: {
+        main: `minmax(auto, 1200px)`,
+        nav: `1fr max-content 1fr`,
+      },
+    },
   },
   plugins: [],
 };
